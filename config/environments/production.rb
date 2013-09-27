b45e8887afc917bf1568d9c0613b80ba7e47ea95
shoheioyama <smallfox27@gmail.com>
2013-09-28 02:47:58 +0900

    今度こそ！

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ PharmkonR::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,6 @@ PharmkonR::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile = true
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
