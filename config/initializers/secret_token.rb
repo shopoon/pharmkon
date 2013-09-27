8e670f33173715d1729c9f9c42546e503eb31250
shoheioyama <smallfox27@gmail.com>
2013-09-28 01:20:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PharmkonR::Application.config.secret_token = '0a117ab59d6934f0b96eefb0b77e1f1deb4d096c21824337fe604544230033435e9de01f441bd9d1c37a85eede6dc7908391a3883e8c22f8fb0b31e75cc55870'
