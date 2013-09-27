0013beae8c931d8be9230998c4819d5582be0ac8
shoheioyama <smallfox27@gmail.com>
2013-09-28 01:30:04 +0900

    devise追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :student_id, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :student_id, :password, :password_confirmation, :remember_me, :department
   # attr_accessible :title, :body
+  #
+  def email_required?
+    false
+  end
 end
