936a424a7567b52ed496318fcc403e5f4b8ea9e5
shoheioyama <smallfox27@gmail.com>
2013-09-28 02:21:47 +0900

    precompile error対策

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ PharmkonR::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( public.js public.css  )
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
