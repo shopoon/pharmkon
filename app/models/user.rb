11a5a42c9de30cfa2ed6f56a8a26b8e38f248370
shoheioyama <smallfox27@gmail.com>
2013-10-15 17:12:21 +0900

    頼む

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,7 +61,7 @@ class User < ActiveRecord::Base
   end
 
   def total_rank
-    @users_total_map ||= self.class.all_user.select('id, total').order('total').map{|user| user.total}
+    @users_total_map ||= self.class.all_user.map{|user| user.total}
     return (@users_total_map.reverse.index(self.total) + 1) rescue nil
   end
 
