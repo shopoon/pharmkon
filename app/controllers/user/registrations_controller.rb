ca986d5c792bb475ee291390dc4b0714e0d7f8af
shoheioyama <smallfox27@gmail.com>
2014-03-18 16:36:51 +0900

    update user without current password

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -12,7 +12,17 @@ class User::RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-    super
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+ 
+    if resource.update_without_current_password(account_update_params)
+      yield resource if block_given?
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -78,6 +78,20 @@ class User < ActiveRecord::Base
     return (@@users_total_map.reverse.index(self.total) + 1) rescue nil
   end
 
+  # allow users to update their accounts without passwords
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+ 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+ 
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+ 
   DEPARTMENT = {
     "薬科学科" => 1,
     "薬学科"   => 2,
