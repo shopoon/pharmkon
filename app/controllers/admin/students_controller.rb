3ce2e1e2c05f3e67390491ed959649e068747e5f
shoheioyama <smallfox27@gmail.com>
2014-03-18 16:52:07 +0900

    admin: student editable

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -9,7 +9,7 @@ class Admin::StudentsController < Admin::ApplicationController
 
   def update
     @student = User.find(params[:id])
-    if @student.update_attributes(params[:student])
+    if @student.update_attributes(params[:user])
       return redirect_to :action => :index, :notice => "更新に成功しました"
     else
       return render :action => :edit, :notice => "更新に失敗しました"
